e2709ac1ef12ee0441bd3abad491b528464e11ef
TaeNontakorn <nontakorlaklame05@gmail.com>
2024-11-29 04:04:51 +0700

    done

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   # Validations
   validates :title, presence: true, length: { maximum: 100 }
   validates :description, presence: true, length: { maximum: 250 }
-  #validates :keyword, presence: true, length: { maximum: 100 }
+  
   validates :user, presence: true
 
   # Active Storage association
